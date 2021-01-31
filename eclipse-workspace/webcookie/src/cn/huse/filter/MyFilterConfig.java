350b931edefe4ecef6a9a686912f26d34f8deaa0
rhc <baitxaps@126.com>
2021-01-31 17:42:47 +0800

    login for test

--- eclipse-workspace/webcookie/src/cn/huse/filter/MyFilterConfig.java
+++ eclipse-workspace/webcookie/src/cn/huse/filter/MyFilterConfig.java
@@ -10,6 +10,22 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+//过滤器的相关配置
+//<url-pattern>的配置
+//完全路径匹配：以/开始   比如/aaa  /aaa/bbb
+//目录匹配	：以/开始 以*结束  比如/*  /aaa/*  /aaa/bbb/*
+//扩展名匹配：不能以/开始 以*开始 比如*.jsp  *.do   *.action
+
+//<servlet-name>的配置,专门以Servlet的配置的名称拦截Servlet。
+
+//<dispatcher>的配置,默认的情况下过滤器会拦截请求。如果进行转发（需要拦截这次转发）。
+//dispatcher的取值
+//REQUEST:默认值,默认过滤器拦截的就是请求。!!!
+//FORWARD:转发!!!
+//INCLUDE:页面包含的时候进行拦截
+//ERROR	:页面出现全局错误页面跳转的时候进行拦截
+
+
 public class MyFilterConfig implements Filter {
 
 	public void destroy() {
