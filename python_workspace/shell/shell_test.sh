185740bf9dd6a87b9744e3e9a22ffb88477bb7bf
rhc <baitxaps@126.com>
2021-03-05 21:54:10 +0800

    shell

---
+++ python_workspace/shell/shell_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "this is shell"
+a="test"
+b="deb"
+echo a,b
+#plugin
+# shellman,shell-format:alt+shrt+f,shellcheck,image preview,file-size
+# Remote - SSH ,Bash Debug
+# code runner
+#short key
+#ctrl+shift+p
